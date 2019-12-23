b9baac3c5554c4c78f9c6f27a0bf3bd427e3bc19
thidarat999 <thidarat_d@kkumail.com>
2019-12-24 00:07:52 +0700

    layouts1

---
+++ Layout/app/src/main/java/com/cis/lab/layout/ConstrantActivity.kt
@@ -0,0 +1,18 @@
+package com.cis.lab.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constrant.*
+
+class ConstrantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrant)
+
+
+        button3.setOnClickListener {
+            this.finish() //จบการทำงานเคลียร์ทุกหน้าออก
+        }
+    }
+}
